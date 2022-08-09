3c49320fa99d00894ec6af6787dd2ce9e604d2f8
Juan Lagos Benitez <lagosben@r16g03.bullx>
2022-08-09 11:00:28 +0300

    ED-1 validation script

--- validation_scripts/validate_effps.py
+++ validation_scripts/validate_effps.py
@@ -1,30 +1,25 @@
 import os
 import logging
+import torch
 import pytorch_lightning as pl
-from pytorch_lightning.callbacks import (
-    EarlyStopping,
-    ModelCheckpoint
-)
-from pytorch_lightning.utilities.model_summary  import ModelSummary
+from pytorch_lightning.utilities.model_summary import ModelSummary
 from detectron2.config import get_cfg
 from detectron2.utils.events import _CURRENT_STORAGE_STACK, EventStorage
 
 
 from efficientps import EffificientPS
 from utils.add_custom_params import add_custom_params
-from utils.dataloader_2_coco_panoptic import dataloader_2_coco_panoptic
 from datasets.vkitti_dataset import get_dataloaders
 
 
 
-def train(args):
+def validate(args):
     
     # Retrieve Config and and custom base parameter
     cfg = get_cfg()
     add_custom_params(cfg)
     cfg.merge_from_file(args.config)
-    cfg.NUM_GPUS = args.ngpus
-    
+
     logging.getLogger("pytorch_lightning").setLevel(logging.INFO)
     logger = logging.getLogger("pytorch_lightning.core")
     if not os.path.exists(cfg.CALLBACKS.CHECKPOINT_DIR):
@@ -38,10 +33,8 @@ def train(args):
 
     #Get dataloaders
     if cfg.DATASET_TYPE == "vkitti2":
-        train_loader, valid_loader, _ = get_dataloaders(cfg)
+        _, valid_loader, _ = get_dataloaders(cfg)
 
-    # print("Converting dataloader to coco_panoptic json")
-    dataloader_2_coco_panoptic(cfg, valid_loader)
     # Create model or load a checkpoint
     if os.path.exists(cfg.CHECKPOINT_PATH):
         print('""""""""""""""""""""""""""""""""""""""""""""""')
@@ -56,32 +49,25 @@ def train(args):
         efficientps = EffificientPS(cfg)
         cfg.CHECKPOINT_PATH = None
 
-    # logger.info(efficientps.print)
-    ModelSummary(efficientps, max_depth=-1)
-    # Callbacks / Hooks
-    early_stopping = EarlyStopping('PQ', patience=30, mode='max')
-    checkpoint = ModelCheckpoint(monitor='PQ',
-                                 mode='max',
-                                 dirpath=cfg.CALLBACKS.CHECKPOINT_DIR,
-                                 save_last=True,
-                                 verbose=True,)
-
-    # Create a pytorch lighting trainer
+    ModelSummary(efficientps, max_depth=-1) 
     trainer = pl.Trainer(
         # weights_summary='full',
         # auto_lr_find=True,
-        log_every_n_steps=2500,
+        log_every_n_steps=250,
         gpus=args.ngpus,
         # distributed_backend='ddp',
         accelerator='ddp',
         num_sanity_val_steps=0,
         # fast_dev_run=True,
-        callbacks=[early_stopping, checkpoint],
         # precision=cfg.PRECISION,
         resume_from_checkpoint=cfg.CHECKPOINT_PATH,
         # gradient_clip_val=0,
         accumulate_grad_batches=cfg.SOLVER.ACCUMULATE_GRAD
     )
     logger.addHandler(logging.StreamHandler())
-    # trainer.tune(efficientps, train_loader, valid_loader)
-    trainer.fit(efficientps, train_loader, val_dataloaders=valid_loader)
+
+    
+    efficientps.eval()
+    with torch.no_grad():
+        trainer.validate(efficientps, dataloaders=valid_loader, ckpt_path=cfg.CHECKPOINT_PATH)
+
